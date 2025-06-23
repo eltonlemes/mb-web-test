e9ab30d223df70ee55b3532f3915a59db39886f1
Elton Lemes <adsesilva@gmail.com>
2025-06-23 15:24:52 -0300

    feat: inicializar frontend Vue.js 3 limpo

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <h1>Vue.js 3 funcionando!</h1>
+  </div>
+</template>
+
+<script setup>
+// Nenhum componente importado
+</script>
+
+<style scoped>
+#app {
+  text-align: center;
+  padding: 2rem;
+}
+</style>

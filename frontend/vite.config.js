9d91a38674a07a028187659f304bbb3f12fc0333
Elton Lemes <adsesilva@gmail.com>
2025-06-23 17:51:19 -0300

    feat: build do frontend configurado e integrado com backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,10 @@ import vue from "@vitejs/plugin-vue";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: "../backend/public",
+    emptyOutDir: true,
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),

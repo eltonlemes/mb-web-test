284fad0301f00753d1582d1265b2d51c5f7cd427
Elton Lemes <adsesilva@gmail.com>
2025-06-23 14:53:00 -0300

    refactor: servidor responde apenas na rota /registration

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,8 @@ const path = require("path");
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, "public")));
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/registration", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(PORT, () => {
